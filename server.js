c085b972f0e6290d35ea6361c8c6f21fc1bcfb79
Minori Hashimoto <minori.fh@gmail.com>
2019-08-06 13:13:33 -0700

    edit relative path in server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../client/build"));
+    app.use(express.static("client/build"));
   }
 
   app.use(session({
