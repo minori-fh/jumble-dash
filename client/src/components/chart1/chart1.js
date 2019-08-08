d435551161ab783e45002f815d336dd4815d8784
Minori Hashimoto <minori.fh@gmail.com>
2019-08-08 16:22:15 -0700

    change colors of chart1 graph

--- client/src/components/chart1/chart1.js
+++ client/src/components/chart1/chart1.js
@@ -23,7 +23,7 @@ class Chart1 extends Component {
         labels: this.props.depts,
         datasets: [{
           data: [this.props.marketing, this.props.hr, this.props.design, this.props.engineering, this.props.sales, this.props.finance, this.props.security],
-          backgroundColor: ['red', 'yellow', 'blue', 'green', 'purple', 'teal', 'orange']
+          backgroundColor: ['#df4343', '#ffb01d', '#b0fff4', 'green', '#6d6d6d', 'teal', '#e9ec45']
         }]
       }
     });
@@ -38,7 +38,7 @@ class Chart1 extends Component {
           labels: this.props.depts,
           datasets: [{
             data: [this.props.marketing, this.props.hr, this.props.design, this.props.engineering, this.props.sales, this.props.finance, this.props.security],
-            backgroundColor: ['red', 'yellow', 'blue', 'green', 'purple', 'teal', 'orange']
+            backgroundColor: ['#df4343', '#ffb01d', '#b0fff4', 'green', '#6d6d6d', 'teal', '#e9ec45']
           }]
         }
       });
@@ -48,7 +48,7 @@ class Chart1 extends Component {
   render() {
     return (
       <div>
-        <canvas className='chart' ref={this.chart1Ref} />
+        <canvas id= 'chart1' className='chart' ref={this.chart1Ref} />
       </div>
     )
   };
