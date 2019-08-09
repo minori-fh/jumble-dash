f95267db0ef0ee116077e2ca6269161c3f677eba
Minori Hashimoto <minori.fh@gmail.com>
2019-08-08 17:19:21 -0700

    edit grid system to append new tasks

--- client/src/components/Task/index.js
+++ client/src/components/Task/index.js
@@ -3,14 +3,16 @@ import { Col, Row } from "../Grid";
 
 function Task(props) {
     return (
-        <Col className="xl3">
+        // <Col className="xl3">
+        <div>
             <p>{props.task}</p>
             <h1>Assignees</h1>
             <div>{props.assignee1}</div>
             <div>{props.assignee2}</div>
             <div>{props.assignee3}</div>
             <div>{props.assignee4}</div>
-        </Col>
+        </div>
+        
     );
 }
 
