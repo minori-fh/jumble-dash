3b19b3eed03c16cda6ebd3b5bebabd3911c027c7
Minori Hashimoto <minori.fh@gmail.com>
2019-08-16 16:09:06 -0700

    add deleteprojects api call

--- client/src/components/ProjectButton/index.js
+++ client/src/components/ProjectButton/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function ProjectButton(props) {
 
     return (
-        <button className="menuBtn" onClick={() => props.click(props.id)} id={props.id} name={props.name} className="sideBtn active"><button id='delete-project'>x</button>{props.name}</button>
+        <button className="menuBtn" onClick={() => props.click(props.id)} id={props.id} name={props.name} className="sideBtn active"><button id='delete-project' onClick={() => props.delete(props.id)}>x</button>{props.name}</button>
     );
 
 } 
